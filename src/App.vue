d68a684fc4cf520df3a63082581efa3385a884e9
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-01-28 04:36:07 +0530

    add: components

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,18 @@
 <template>
-  <h2>Volume (0-20): {{ volume }}</h2>
-  <button @click="volume += 2">Increase</button>
-  <button @click="volume -= 2">Decrease</button>
-  <input type="text" v-model="movie">
-
-  <input type="number" v-model="info.age">
-  <input type="text" v-model="info.name">
+  <Greet />
 </template>
 
 <script>
+import Greet from './components/Greet.vue';
 export default {
   name: 'App',
+  components: {
+    Greet,
+  },
   data() {
     return {
-      volume: 0,
-      movie: 'Batman',
-      info: {
-        age: null,
-        name: ''
-      }
+      
     };
-  },
-  watch: {
-    volume(newValue, oldValue) {
-      if (newValue > oldValue && newValue === 16) {
-        alert('Increasing Volume my damage hearing')
-      }
-    },
-    movie: {
-      handler(newValue) {
-        console.log(`Calling API Call with movie: ${newValue}`);
-      },
-      immediate: true // Runs on page reload too
-    },
-    info: {
-      handler(newValue) {
-        console.log(`Info: Age = ${newValue.age}, Name = ${newValue.name}`)
-      },
-      deep: true //* Generally watchers aren't called if deep properties change in an object or array. If we use .concat, and change the array itself, deep true not required as whole array got changed
-    }
   }  
 }
 </script>
---
+++ src/components/Greet.vue
@@ -0,0 +1,11 @@
+<template>
+    <h2>Hello Varun</h2>
+</template>
+
+<script>
+export default {
+    name: "Greet"
+}
+</script>
+
+<style scoped></style>
