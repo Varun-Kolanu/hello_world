2dd326a3fdf16b880422718bb59b20a5825886dd
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-01-28 02:59:05 +0530

    Form Handling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,55 @@
 <template>
-  <h2>{{ count }}</h2>
-  <button v-on:click="count += 1">Increment</button>
-  <button @click="decrement">Decrement</button> <!-- shorthand for v-on: is @ -->
-  <button @click="clickHandler">Click</button>
-  <button @click="clickHandler2(1,$event)">Click2</button>
-  <button @click="decrement(), clickHandler($event)">Multiple methods</button> <!-- You should call the functions if are passing multiple methods and pass $event if event is used -->
+  <div>
+    <pre>
+      {{ JSON.stringify(formValues, null, 2) }}
+    </pre>
+  </div>
+  <form @submit.prevent="submitHandler"> <!-- or by using event.preventDefault() -->
+    <div>
+      <label for="name">Name</label>
+      <input type="text" placeholder="Name" id="name" v-model="formValues.name"> <!-- v-model for 2 way data binding -->
+    </div>
+    <div>
+      <label for="profile">Profile</label>
+      <textarea name="profile" id="profile" cols="30" rows="10" v-model="formValues.profile"></textarea>
+    </div>
+    <div>
+      <label for="country">Country</label>
+      <select name="country" id="country" v-model="formValues.country">
+        <option value="">Select</option>
+        <option value="India">India</option>
+        <option value="China">China</option>
+      </select>
+    </div>
+    <div>
+      <label for="job-location">Job Location</label>
+      <select name="job-location" id="country" multiple v-model="formValues.jobLocation">
+        <option value="India">India</option>
+        <option value="China">China</option>
+      </select>
+    </div>
+    <div>
+      <label for="major">Major?</label>
+      <input type="checkbox" id="major" v-model="formValues.major">
+    </div>
+    <div>
+      <label for="remote">Open to remote work?</label>
+      <input type="checkbox" id="remote" v-model="formValues.remote" true-value="yes" false-value="no">
+    </div>
+    <div>
+      <label for="html">Html</label>
+      <input type="checkbox" id="html" v-model="formValues.skills" value="html">
 
+      <label for="css">CSS</label>
+      <input type="checkbox" id="css" v-model="formValues.skills" value="css">
 
+      <label for="js">JS</label>
+      <input type="checkbox" id="js" v-model="formValues.skills" value="js">
+    </div>
+
+    <button type="submit">Submit</button>
+  </form>
+  <button @click="formValues.name = 'Varun'">Click</button>
 </template>
 
 <script>
@@ -14,18 +57,20 @@ export default {
   name: 'App',
   data() {
     return {
-      count: 0
+      formValues: {
+        name: '',
+        profile: '',
+        country: '',
+        jobLocation: [],
+        major: false,
+        remote: "no",
+        skills: []
+      }
     };
   },
   methods: {
-    decrement() {
-      this.count -= 1
-    },
-    clickHandler(event) {
-      console.log(event) //event is automatically passed if no args passed
-    },
-    clickHandler2(x,event) {
-      console.log(event) // $event needs to be passed if args passed
+    submitHandler() {
+      console.log(this.formValues)
     }
   }
 }
