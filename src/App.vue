79485e9ffedf572d64ea3fdacfa71e5d075710d9
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-01-27 17:36:58 +0530

    v-bind to change attributes dynamically by plain, array and object

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
-<div> {{ greet }} {{ name }} </div>
-<div v-html="branch"></div> <!-- replaces whole html of v-html into the div's inner html and be cautious -->
+<div v-bind:id="heading">Heading</div> <!-- binds variable to an attribute such as id, disabled -->
+<button :disabled="isDisabled">Bind</button> <!-- shorthand for v-bind -->
+<h2 :class="status">Status</h2>
+<h2 :class="isPromoted && 'promoted'">Promoted</h2>
+<h2 :class="isSoldOut ? 'sold-out' : 'exists'">Sold Out ?</h2>
+<h2 :class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'exists']">Array Conditional</h2>
+<h2 :class="{
+  promoted: isPromoted,
+  exists: !isSoldOut,
+  'sold-out': isSoldOut
+}">Object binding</h2>
 </template>
 
 <script>
@@ -10,7 +19,12 @@ export default {
     return {
       greet: "Hello",
       name: "Varun",
-      branch: "<b>CSE</b>"
+      branch: "<b>CSE</b>",
+      heading: "heading",
+      isDisabled: true,
+      status: "success",
+      isPromoted: true,
+      isSoldOut: false
     };
   }
 }
@@ -24,5 +38,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.success {
+  background-color: green;
+}
+
+.promoted {
+  background-color: lightblue;
+}
+
+.sold-out {
+  color: red;
+}
+
+.exists {
+  color: green;
 }
 </style>
