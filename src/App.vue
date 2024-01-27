4a0d99fa351fa73e69f38bb1e2a1ab541597e53d
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-01-28 01:23:20 +0530

    Methods

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <!-- should not mix v-if with v-for because v-if is computed before v-for. So use template tag -->
-
-  <!-- Wrong: -->
-  <!-- <div v-for="name in names" :key="name" v-if="name === 'Varun'">{{ name }}</div> -->
-
-  <!-- Correct: -->
-  <template v-for="name in names" :key="name" >
-    <div v-if="name === 'Varun'">{{ name }}</div>
-  </template>
+  <h2>{{ 1 + 2 + 3 }}</h2>
+  <h2>Add method: {{ add(1,2,3) }}</h2>
+  <h2>Multiply: {{ multiplier(2,4) }}</h2>
 </template>
 
 <script>
@@ -17,6 +11,15 @@ export default {
     return {
       
     };
+  },
+  methods: {
+    add(a,b,c) {
+      return a+b+c
+    },
+    multiplier: (x,y) => {
+      return x*y
+    }
+    // If you use arrow function, cn't use this keyword to access data
   }
 }
 </script>
